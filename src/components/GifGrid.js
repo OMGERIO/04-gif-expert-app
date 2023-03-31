ab7227ef280501d0c2209a2131e08388629f5fc1
Erio <eriocedeno@gmail.com>
2023-03-30 21:25:50 -0400

    modified:   src/components/AddCategory.js 	modified:   src/components/GifGrid.js 	new file:   src/components/GifGridItem.js 	new file:   src/helpers/getGifs.js 	modified:   src/index.css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,38 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
     // const apiKey = 'PZK2gYzhvd6eerXCx3mm3v1UdmLi5qSo'; 
 
-    const [count, setCount] = useState(0);
+    const [images, setimages] = useState([]);
+
     useEffect(()=>{
-        getGifs();
+        getGifs(category).then( imgs => setimages(imgs));
     },[])
 
 
 
-    const getGifs = async () =>{
-        const query = 'Goku'
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=${query}&limit=10&api_key=PZK2gYzhvd6eerXCx3mm3v1UdmLi5qSo'
-        const resp = await fetch(url);
-        const data = await resp.json();
-        
-        const gifs = data.map(img =>{
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
-        console.log(gifs);
-    } 
+    
 
 
     return (
-        <div>
+        <>
             <h3>{category}</h3>
-            
-        </div>
+            <div className='card-grid'>
+                {
+                    images.map( gif => {
+                        <GifGridItem 
+                            key= {gif.id}   
+                            {...gif}
+                        />;    
+                    })
+                }
+            </div>
+        </>
+       
     )
 }
